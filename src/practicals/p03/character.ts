6ebe35cb5c2dd02b3f098a7590c33391c4622460
txasw <txasw@outlook.co.th>
2026-02-20 17:50:12 +0700

    003 - 01

--- src/practicals/p03/character.ts
+++ src/practicals/p03/character.ts
@@ -1,1 +1,22 @@
-export class Character {}
+export class Character {
+    public name: string = ""
+    public health: number = 100
+
+    constructor(name: string = "", health: number = 100) {
+        this.name = name
+        this.health = health
+    }
+
+    getName() {
+        return this.name
+    }
+
+    getHealth() {
+        return this.health
+    }
+
+    receiveDamage(damage: number = 0){
+        const current = this.health
+        this.health = current - damage
+    }
+}
--- src/practicals/p03/warrior.ts
+++ src/practicals/p03/warrior.ts
@@ -1,5 +1,19 @@
 import { Character } from "./character";
 
 export class Warrior extends Character {
-  // TODO: implement class properties, constructor with super(...), and methods
+  public weapon: string = ""
+  
+  constructor(name: string = "", weapon: string = "", health: number = 100) {
+    super(name, health);
+    this.weapon = weapon;
+  }
+
+  getWeapon(){
+    return this.weapon
+  }
+
+  receiveDamage(damage: number = 0) {
+    const current = this.health
+        this.health = current - (damage - damage*0.1)
+  }
 }
