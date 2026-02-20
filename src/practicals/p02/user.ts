4a99d946771b535b211c765d2a1d1babeca2ba54
txasw <txasw@outlook.co.th>
2026-02-20 17:39:36 +0700

    02 - 001

--- src/practicals/p02/user.ts
+++ src/practicals/p02/user.ts
@@ -1,1 +1,34 @@
-export class User {}
+export class User {
+    private age: number
+    public firstname: string = ""
+    public lastname: string = ""
+
+    public static BIRTH_YEAR = 2000
+
+    constructor(firstname: string, lastname: string, age: number = 0) {
+        this.firstname = firstname
+        this.lastname = lastname
+        this.age = age
+    }
+
+    setFirstname(firstname: string) {
+        this.firstname = firstname
+    }
+
+    setLastname(lastname: string) {
+        this.lastname = lastname
+    }
+
+    setAge(age: number){
+        this.age = age
+    }
+
+    getFullName() {
+        return `${this.firstname} ${this.lastname}`
+    }
+
+    getAge() {
+        return this.age
+    }
+
+}
