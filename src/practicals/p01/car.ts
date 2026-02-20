ce4a982ed78400887e04513d0e09c361daf8296d
txasw <txasw@outlook.co.th>
2026-02-20 17:26:49 +0700

    001

--- src/practicals/p01/car.ts
+++ src/practicals/p01/car.ts
@@ -1,1 +1,25 @@
-export class Car {}
+export class Car {
+    private brand: string;
+    private model: string;
+
+    constructor(brand: string, model: string) {
+        this.brand = brand;
+        this.model = model;
+    }
+
+    start() {
+        console.log(`${this.brand} ${this.model} is starting...`);
+    }
+
+    accelerate() {
+        console.log(`${this.brand} ${this.model} is accelerating...`);
+    }
+
+    brake() {
+        console.log(`${this.brand} ${this.model} is braking...`);
+    }
+
+    stop() {
+        console.log(`${this.brand} ${this.model} is stopping...`);
+    }
+}
